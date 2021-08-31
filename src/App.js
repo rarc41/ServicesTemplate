4fcebf037086db46c6599476dc8231a254cc4780
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-08-31 00:24:23 -0500

    Navigation with material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import Header from "./components/Header";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#9fffe0',
+      main: '#69f0ae',
+      dark: '#2bbd7e',
+      contrastText: '#fb8c00',
+    },
+    secondary: {
+      light: '#ffe97d',
+      main: '#ffb74d',
+      dark: '#c88719',
+      contrastText: '#000000',
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      
+      <ThemeProvider theme={theme}>
+        <Header theme={theme}></Header>
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,82 @@
-import React, { Fragment } from "react";
-import {v4 as uuidv4} from 'uuid';
-import { Button } from '@material-ui/core';
+import React, { Fragment, useState } from "react";
+// import { v4 as uuidv4 } from "uuid";
+import {
+  Typography,
+  Grid,
+  Tab,
+  Tabs,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1),
+    fontSize: 25,
+    textTransform: "capitalize",
+  },
+  navegacion: {
+    display: "flex",
+    marginTop: "2rem",
+    justifyContent: "space-evenly",
+    backgroundColor: "#3C5FC7",
+  },
+
+  pestañas: {
+    color: "white",
+    fontWeight: "bold",
+    opacity: 1,
+  },
+  nav_bar: {
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
+
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const homeInfo = {
     lead: "Robin Angel Romero Cordero",
     profession: "Developer",
     opciones: ["Inicio", "Sobre mi", "Clientes", "Proyectos"],
   };
 
-  let {opciones} = homeInfo
+  let { opciones } = homeInfo;
 
   return (
     <Fragment>
       <header>
-        <Button>
-          Robin Angel Romero <span>Freelancer</span>
-        </Button>
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography className={classes.root}>{homeInfo.lead}</Typography>
+
+          <Typography>{homeInfo.profession}</Typography>
+        </Grid>
       </header>
-      <div >
-        <nav >
+      <div
+      // className={classes.navegacion}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          className={classes.nav_bar}
+          variant="fullWidth"
+          indicatorColor="secondary"
+          textColor="secondary"
+        >
           {opciones.map((opcion) => (
-            <a key={uuidv4()}>{`${opcion}`}</a>
+            <Tab key={opciones.indexOf(opcion)} label={opcion}></Tab>
           ))}
-        </nav>
+        </Tabs>
       </div>
     </Fragment>
   );
